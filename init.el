fbb3e382ac9a72137825a5629db7a4189b678034
qazwsxedc121 <qazwsxedc121@gmail.com>
2013-07-24 17:53:58 +0800

    Removed places and cookies.Added highlight-indentation/indent-guide/tabbar/tabbar-ruler.

--- init.el
+++ init.el
@@ -3,14 +3,22 @@
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
+(require 'sr-speedbar)
 (load-theme 'zenburn t)
+(add-hook 'speedbar-mode-hook '(lambda () (linum-mode -1)))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes (quote ("9f443833deb3412a34d2d2c912247349d4bd1b09e0f5eaba11a3ea7872892000" default)))
+ '(global-auto-revert-mode t)
+ '(global-linum-mode t)
+ '(global-whitespace-mode nil)
+ '(line-number-mode t)
+ '(show-trailing-whitespace t)
  '(speedbar-default-position (quote left-right))
+ '(speedbar-show-unknown-files t)
  '(speedbar-supported-extension-expressions (quote (".[ch]\\(\\+\\+\\|pp\\|c\\|h\\|xx\\)?" ".tex\\(i\\(nfo\\)?\\)?" ".el" ".emacs" ".l" ".lsp" ".p" ".java" ".js" ".f\\(90\\|77\\|or\\)?" ".ad[abs]" ".p[lm]" ".tcl" ".m" ".scm" ".pm" ".py" ".g" ".s?html" ".ma?k" "[Mm]akefile\\(\\.in\\)?" ".clj" ".json" ".css")))
  '(sr-speedbar-right-side nil))
 (custom-set-faces
