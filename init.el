96139c488a934e7225525ec41b608e2a94f8c692
guoxc <qazwsxedc121@gmail.com>
2013-07-30 09:13:20 +0800

    Init frame & speedbar & tabbar

--- init.el
+++ init.el
@@ -1,11 +1,26 @@
+;;init package system
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
+
+;;speedbar default open
 (require 'sr-speedbar)
-(load-theme 'zenburn t)
 (add-hook 'speedbar-mode-hook '(lambda () (linum-mode -1)))
+(add-hook 'after-init-hook '(lambda () (sr-speedbar-toggle)))
+
+;;load theme
+(load-theme 'zenburn t)
+
+;;tabbar default open
+(add-hook 'after-init-hook '(lambda () (tabbar-ruler-up)))
+
+;;set up frame size
+(set-frame-position (selected-frame) 10 10)
+(set-frame-width (selected-frame) 140)
+(set-frame-height (selected-frame) 50)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -20,7 +35,9 @@
  '(speedbar-default-position (quote left-right))
  '(speedbar-show-unknown-files t)
  '(speedbar-supported-extension-expressions (quote (".[ch]\\(\\+\\+\\|pp\\|c\\|h\\|xx\\)?" ".tex\\(i\\(nfo\\)?\\)?" ".el" ".emacs" ".l" ".lsp" ".p" ".java" ".js" ".f\\(90\\|77\\|or\\)?" ".ad[abs]" ".p[lm]" ".tcl" ".m" ".scm" ".pm" ".py" ".g" ".s?html" ".ma?k" "[Mm]akefile\\(\\.in\\)?" ".clj" ".json" ".css")))
- '(sr-speedbar-right-side nil))
+ '(sr-speedbar-right-side nil)
+ '(tabbar-ruler-global-ruler t)
+ '(tabbar-ruler-global-tabbar t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
