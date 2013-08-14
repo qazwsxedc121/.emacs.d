e93f51a8bfdd5143b917679628c6ee45785fd8dd
qazwsxedc121 <qazwsxedc121@gmail.com>
2013-08-14 19:30:02 +0800

    Updated && added yasnippet

--- elpa/magit-20130720.1549/magit-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "magit" "20130720.1549" "control Git from Emacs [github]" 'nil)
-
--- elpa/org-20130720.1353/org-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "org" "20130720.1353" "Outline-based notes management and organizer [git]" 'nil)
-
